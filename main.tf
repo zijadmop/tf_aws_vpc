e2859341bbef0c436be0afe54cf73af26692302d
Zijad Purkovic <zpurkovic@ministryofprogramming.com>
2016-05-11 14:47:10 +0200

    Testing NAT gateways resources for private subnets

--- main.tf
+++ main.tf
@@ -25,6 +25,30 @@ resource "aws_route_table" "private" {
   tags { Name = "${var.name}-private" }
 }
 
+################################################################################
+resource "aws_route" "nat_gateway" {
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = "${element(aws_nat_gateway.nat.*.id, count.index)}"
+  count                  = "${length(split(",", var.nat_gateways_count))}"
+
+  depends_on             = ["aws_route_table.private"]
+}
+
+# NAT
+resource "aws_eip" "nat" {
+  vpc   = true
+  count = "${var.nat_gateways_count}"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
+  subnet_id     = "${element(split(",", var.public_subnet_ids), count.index)}"
+  count         = "${var.nat_gateways_count}"
+}
+
+################################################################################
+
 resource "aws_subnet" "private" {
   vpc_id = "${aws_vpc.mod.id}"
   cidr_block = "${element(split(",", var.private_subnets), count.index)}"
--- variables.tf
+++ variables.tf
@@ -11,3 +11,10 @@ variable "enable_dns_support" {
   description = "should be true if you want to use private DNS within the VPC"
   default = false
 }
+
+// Since Terraform doesn't yet support conditional logic, adding count = 0 to a
+// resource will disable it, anything else will enable the resource
+variable "nat_gateways_count" {
+  description = "defaults to 0, set to 1 to enable NAT gateway for private subnets"
+  default = 0
+}
